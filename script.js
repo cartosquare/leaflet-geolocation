f08eb0c46d0c5aaaf6de0523102d9993272ae2bc
XiangXu <xuxiang@sensetime.com>
2020-02-05 21:37:41 +0800

    switch basemap

--- script.js
+++ script.js
@@ -15,8 +15,14 @@ var mapbox = new L.TileLayer(mapboxUrl, {
   zoomOffset: -1
 });
 
+layer = new L.tileLayer('http://{s}.tile.stamen.com/toner/{z}/{x}/{y}.png', {
+            minZoom: 8,
+            maxZoom: 18,
+            attribution: '&copy; 病例数据：<a href="http://wjw.gz.gov.cn/ztzl/xxfyyqfk/yqtb/">广州市卫健委</a>，<a href="http://wjj.foshan.gov.cn/zwgk/gsgg/tzgg/">佛山市卫健委</a>，<a href="http://wjw.sz.gov.cn/yqxx/index_1.htm">深圳市卫健委</a>；底图设计：<a href="https://stamen.com/">Stamen Design</a>；地图数据：<a href="https://www.openstreetmap.org/">OpenStreetMap</a>'
+        });
+
 var map = new L.Map('map', {
-    layers: [mapbox],
+    layers: [layer],
     center: [51.505, -0.09],
     zoom: 10,
     zoomControl: true
